8f80943f23fb8a857624bdc9126ec29899e9ecdd
rsparano <Rsparano@charter.net>
2019-02-12 13:36:24 -0700

    Add files via upload

--- EmailSender.java
+++ EmailSender.java
@@ -1,11 +1,19 @@
 package rsparano;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+
 import java.util.Properties;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMultipart;
 
 /**
 * Java Program to send text mail using default SMTP server and without authentication.
@@ -30,11 +38,34 @@ public class EmailSender{
             message.setFrom(new InternetAddress(from));                    // setting header fields
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
             message.setSubject("Test Mail from Java Program"); // subject line
-
+         
+            // Create the message part
+            BodyPart messageBodyPart = new MimeBodyPart();
+            
+            // Now set the actual message
+            messageBodyPart.setText("This is message body");
+            
             // actual mail body
-            message.setText("You can send mail from Java program by using mail API, but you need"
-                    + "couple of more JAR files e.g. smtp.jar and activation.jar");
+            //message.setText("You can send mail from Java program by using mail API, but you need"
+              //      + "couple of more JAR files e.g. smtp.jar and activation.jar");
+         
+            // Create a multipar message
+            Multipart multipart = new MimeMultipart();
+         
+            
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+            
+            // Part two is attachment
+            messageBodyPart = new MimeBodyPart();
+            String filename = "C:\\Temp\\File.txt";
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            multipart.addBodyPart(messageBodyPart);
 
+            // Send the complete message parts
+            message.setContent(multipart);
             // Send message
             Transport.send(message);
             System.out.println("Email Sent successfully....");
